8a391029f1bc3a508d9d99b8ff9974ec21bf38f6
Farbod Parkhuee <farbod.p1390@gmail.com>
2024-02-21 07:04:43 -0800

    Create get_info function

--- script.py
+++ script.py
@@ -1,7 +1,14 @@
 from tkinter import * 
 from colorama import Fore, Back, Style, init
-import platform, os, random
+import platform, os, random, time
 init(autoreset=True)
+class msfauto_ultra():
+    def __init__(self, app_name, ip, port_one, port_two, payload):
+        self.app_name = app_name
+        self.ip = ip
+        self.port1 = port_one
+        self.port2 = port_two
+        self.payload = payload
 def banner():
     banners = {
         1 : f"""{Fore.CYAN}
@@ -55,6 +62,37 @@ __|_________\______/      /  ░▒█░░▒█░▒█▄▄▄█░▒█
 """
     }
     print(banners[random.randint(1, 3)])
+def clear(): os.system("clear")
 def check_os():
     if platform.uname()[0] != "Linux": print(Fore.RED + Style.BRIGHT + "[-] Your platform must be Kali Linux."), exit()
     else: print(Fore.GREEN + Style.BRIGHT + "[+] OS Accepted! Continue...")
+    time.sleep(5)
+    clear()
+def get_info():
+    root = Tk()
+    Label(root, text="your application's name:").place(x=15, y=47)
+    Label(root, text="your ip:").place(x=15, y=97)
+    Label(root, text="your first port:").place(x=15, y=147)
+    Label(root, text="your srconf port:").place(x=15, y=197)
+    Label(root, text="your payload:").place(x=15, y=247)
+    Label(root, text="(android, windows, or mac)").place(x=15, y=275)
+    app_name = Entry(root, width=50)
+    ip = Entry(root, width=50)
+    port1 = Entry(root, width=50)
+    port2 = Entry(root, width=50)
+    payload = Entry(root, width=50)
+    app_name.place(x=150, y=50)
+    ip.place(x=150, y=100)
+    port1.place(x=150, y=150)
+    port2.place(x=150, y=200)
+    payload.place(x=150, y=250)
+    def save():
+        with open("config", "w") as writer:
+            writer.write(f"{app_name.get()},{ip.get()},{port1.get()},{port2.get()},{payload.get()}")
+    Button(root, command=save, text="submit", width=8, height=2).place(x=300, y=290)
+    root.geometry("500x350")
+    root.title("Get informations")
+    root.resizable(False, False)
+    root.mainloop()
+
+get_info()
