da0e865d942ee87f1e758e525d6aeb567ee38436
Farbod Parkhuee <farbod.p1390@gmail.com>
2024-02-22 03:08:23 -0800

    write codes in console.py

---
+++ console.py
@@ -0,0 +1,7 @@
+from script import * 
+# This code converted easy for everyone!
+# if you'r not a programmer, you can read this code!
+# use it for fun!
+banner()
+msfultra = read_info()
+msfultra.start_msfconsole()
